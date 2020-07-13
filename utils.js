364f7801aef9b528fbc74890a63b80ff60c84478
jiwen xin <kevinxin@scripps.edu>
2020-07-13 09:25:11 -0700

    use find-common-path package in  utils

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const jsonata = require("jsonata");
 const _ = require("lodash");
+const commonpath = require("common-path-prefix");
 
 /**
  * Extract all paths from a template object
@@ -20,40 +21,22 @@ function extractPathsFromTemplate(template) {
  * @returns - the longest common path(separated by dot)
  */
 function findLongestCommonPath(paths) {
-    let new_paths = [];
+    if (!(Array.isArray(paths))) {
+        return null
+    }
     for (let item of paths) {
         if (typeof item !== "string") {
             return null
-        } else {
-            new_paths.push(item.split('.'));
         }
     }
-    if (new_paths.length === 1) {
+    if (paths.length === 1) {
         return paths[0]
-    } else if (new_paths.length === 0) {
-        return null
     }
-    let common_path = [];
-    let eql;
-    for (let i = 0; i < new_paths[0].length; i++) {
-        eql = false;
-        for (let item of new_paths) {
-            if (item[i] && item[i] === new_paths[0][i]) {
-                eql = true;
-            } else {
-                eql = false;
-            }
-        }
-        if (eql) {
-            common_path.push(new_paths[0][i]);
-        } else {
-            break;
-        }
-    }
-    if (common_path.length === 0) {
-        return null;
+    let common_path = commonpath(paths, '.');
+    if (common_path.endsWith('.')) {
+        return common_path.slice(0, -1)
     } else {
-        return (common_path.join('.'));
+        return null
     }
 }
 
