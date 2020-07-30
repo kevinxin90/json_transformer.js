ac7f24b1c1594738d24453e3ac095f93b14f4e94
jiwen xin <kevinxin@scripps.edu>
2020-07-30 16:11:46 -0700

    add test for nested template

--- test/index.js
+++ test/index.js
@@ -54,4 +54,18 @@ describe("test main function", () => {
         let res = transform(json_doc, template);
         expect(res).to.be.an("object").deep.equals({ 'ensembl': 1017, 'pathway': [{ 'id': 'WP123', 'name': 'aaa' }, { 'id': 'WP1234', 'name': 'aaaa' }] });
     })
+
+    it("test nested template and simple object", () => {
+        let json_doc = {
+            query: 'HP:0000791',
+            _id: 'HP:0000791',
+            _score: 1,
+            parents: ['HP:0000787']
+        };
+        let template = { subclass_of: { HP: 'parents' } };
+        let res = transform(json_doc, template);
+        expect(res).to.have.property("subclass_of");
+        expect(res["subclass_of"]).to.have.property("HP");
+        expect(res["subclass_of"]["HP"][0]).to.equal("HP:0000787");
+    })
 })
